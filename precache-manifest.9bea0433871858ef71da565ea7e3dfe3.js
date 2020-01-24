02f7ddefdda2a0c96d7fd8e29bbb8de13d15d786
jlelito <joshualelito@gmail.com>
2020-01-24 09:33:31 -0500

    Updates

---
+++ precache-manifest.9bea0433871858ef71da565ea7e3dfe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7aabbeac33c93152b5d47294122b810",
+    "url": "/lottery/index.html"
+  },
+  {
+    "revision": "71641c0b8123f6030418",
+    "url": "/lottery/static/css/2.c0f5ac91.chunk.css"
+  },
+  {
+    "revision": "71641c0b8123f6030418",
+    "url": "/lottery/static/js/2.1f1613cb.chunk.js"
+  },
+  {
+    "revision": "99c5655d2432b8199a89e031b7b8d3f7",
+    "url": "/lottery/static/js/2.1f1613cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3924221dae0c9b5a15e7",
+    "url": "/lottery/static/js/main.68dfd50c.chunk.js"
+  },
+  {
+    "revision": "83df882e8cc2a73c84e0",
+    "url": "/lottery/static/js/runtime-main.2782794a.js"
+  }
+]);
